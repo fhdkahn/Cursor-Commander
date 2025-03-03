756c23cb787300b525556618a6ef26e7b1c70e46
fhdkahn <155093991+fhdkahn@users.noreply.github.com>
2025-03-03 14:51:43 +0400

    Initial Commit

---
+++ CursorCommander/CursorCommanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorCommanderApp.swift
+//  CursorCommander
+//
+//  Created by Mac on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CursorCommanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CursorCommanderTests/CursorCommanderTests.swift
@@ -0,0 +1,17 @@
+//
+//  CursorCommanderTests.swift
+//  CursorCommanderTests
+//
+//  Created by Mac on 03/03/2025.
+//
+
+import Testing
+@testable import CursorCommander
+
+struct CursorCommanderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
